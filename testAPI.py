a2205c3bba3e883a62d8d52a6afce4235a988659
Amar Singh <68085422+asinghh1@users.noreply.github.com>
2024-01-11 19:33:09 -0800

    starter code finished

--- testAPI.py
+++ testAPI.py
@@ -2,20 +2,13 @@ from nba_api.stats.static import players
 from nba_api.stats.static import teams
 from nba_api.stats.endpoints import commonteamroster
 
-# Find players by full name.
-#print(players.find_players_by_full_name('james'))
+#provides csv of all players of a certain team in the nba with important stats
 
-# Find players by first name.
-holder = players.find_players_by_first_name('lebron')
-holder.append(players.find_players_by_last_name('durant'))
-
-#print(holder[0]['id'])
-
-#career = playercareerstats.PlayerCareerStats(holder[0]['id']) 
-
+#put team name here
 team = teams.find_teams_by_city('New Orleans')
 roster = commonteamroster.CommonTeamRoster(team[0]['id'])
 df = roster.get_data_frames()[0]
 
+df = df[["TeamID", "PLAYER", "POSITION", "AGE", "EXP", "PLAYER_ID"]]
+
 df.to_csv('PelsAPI.csv', index=False)
-# Find players by last name.
