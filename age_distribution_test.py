8fe7f6ec3ac2b35f2366787098b0dc23cba6c2f1
RPadhi678 <116604147+RPadhi678@users.noreply.github.com>
2024-02-21 18:47:31 -0800

    Final age_distribution_test.py

--- age_distribution_test.py
+++ age_distribution_test.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot as plt
 
 def process_team_data(team_name):
     # Construct file names from the team name
-    salary_file = f"{team_name}Test.csv"
+    salary_file = f"{team_name}Salary.csv"
     roster_file = f"{team_name}Roster.csv"
+    standings_file = "NBAStandings.csv"
+    standings_data = pd.read_csv(standings_file)
 
     # Load and process the Salary dataset
     try:
@@ -39,6 +41,9 @@ def process_team_data(team_name):
             age_groups['29-33'] += salary
         else:  # Age is over 33
             age_groups['Over 33'] += salary
+    
+    # Filter out age groups with a salary of $0
+    age_groups = {key: val for key, val in age_groups.items() if val > 0}
 
     # Custom autopct function to show percentage and money value
     def autopct_format(values):
@@ -47,11 +52,25 @@ def process_team_data(team_name):
             val = int(round(pct*total/100.0))
             return '{p:.2f}%\n(${v:,})'.format(p=pct,v=val)
         return my_format
+    
+    # Extract only the last name of each team from standings data
+    standings_data['Team'] = standings_data['Team'].str.split().str[-1]
+
+    # Matches the team name input with the input that is on the standings
+    standing_index = standings_data[standings_data['Team'].str.lower() == team_name.lower()].index
+    if len(standing_index) == 0:
+        print(f"Team record not found for {team_name}.")
+        return
+
+    # Extract team's record by the row index wherever the team name is
+    team_record = standings_data.iloc[standing_index[0]]
+
+    team_record_str = f"{team_record['Team']}'s record: {team_record['Overall']}"
 
     # Plotting the pie chart for age groups with percentage and money value
-    plt.figure(figsize=(8, 8))
+    plt.figure(figsize=(10, 10))
     plt.pie(age_groups.values(), labels=age_groups.keys(), autopct=autopct_format(age_groups.values()))
-    plt.title(f'Salary Distribution by Age Group for {team_name}\nTotal Salary: ${total_salary:,}')
+    plt.title(f'Salary Distribution by Age Group for {team_name}\nTotal Salary: ${total_salary:,}\n{team_record_str}')
     plt.show()
 
     # Load and display the Roster dataset
@@ -64,6 +83,8 @@ def process_team_data(team_name):
 # Example usage
 team_name = input("Please enter the team name: ")
 process_team_data(team_name)
+
+
 
 
 # Asking the user for the team name
